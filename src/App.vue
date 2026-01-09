9115b855cb43caea5a3c7f3e3a79f1bc1c7b1c54
三告 <986843038@qq.com>
2026-01-09 17:31:01 +0800

    gsap

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
-import HorizontalScrollEffect from "./components/HorizontalScrollEffect.vue";
+import GsapFirstAnimation from "./components/GsapFirstAnimation.vue";
 </script>
 
 <template>
-  <HorizontalScrollEffect></HorizontalScrollEffect>
+  <GsapFirstAnimation></GsapFirstAnimation>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/GsapFirstAnimation.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="gsap-first-animation">
+    <div class="box"></div>
+    <button @click="animGsapTo">GsapTo</button>
+    <button @click="animGsapFrom">GsapFrom</button>
+    <button @click="animGsapFromTo">GsapFromTo</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import gsap from "gsap";
+function animGsapTo() {
+  gsap.to(".box", { y: 200, fill: "#234143" });
+}
+function animGsapFrom() {
+  gsap.from(".box", { x: 200 });
+}
+function animGsapFromTo() {
+  gsap.fromTo(".box", { x: 200 }, { y: 100 });
+}
+</script>
+
+<style lang="css" scoped>
+.gsap-first-animation {
+  width: 100vw;
+  height: 100vh;
+  background: #000;
+  position: relative;
+  .box {
+    width: 100px;
+    height: 100px;
+    border-radius: 10px;
+    background: greenyellow;
+    position: absolute;
+    top: 300px;
+    left: 500px;
+  }
+}
+</style>
