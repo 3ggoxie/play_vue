947187f7b8b6e1ce8833bc6427f88b941b5274d9
三告 <986843038@qq.com>
2026-02-06 17:30:51 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import GsapFirstAnimation from "./components/GsapFirstAnimation.vue";
-</script>
-
 <template>
-  <GsapFirstAnimation></GsapFirstAnimation>
+  <!-- <GsapFirstAnimation></GsapFirstAnimation> -->
+  <GsapStagger />
 </template>
+<script setup lang="ts">
+import GsapStagger from "./components/GsapStagger.vue";
+
+// import GsapFirstAnimation from "./components/GsapFirstAnimation.vue";
+</script>
 
 <style scoped></style>
